aa27aa7b772029f8701705ffbd5df3bde9a39d55
Andrew Miller <thedr111111@yahoo.com>
2020-08-04 12:46:34 -0700

    Imported Assets
    Imported Enviroment Assets and Code from Demo Project.

---
+++ 2dARPG/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	//Variables
+	GameObject player; //the player's gameobject
+	Vector3 offset;    //offset for the camera follow
+
+	void Awake(){
+		//finds the gameobject in the scene with the Player tag and assigns it
+		player = GameObject.FindGameObjectWithTag ("Player");
+		//initial offset is set
+		offset = transform.position - player.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//remember transform (lowercase) refers to the transform of the game object that this script is attached to.
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ 2dARPG/Assets/Scripts/PlayerController2d.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This is a 2d Character controller using the CharacterController component. 
+//I used the Unity Documentation here: https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+//as a base.
+//
+
+
+public class PlayerController2d : MonoBehaviour
+{
+	private CharacterController controller;
+	public Vector3 playerVelocity;
+	private bool groundedPlayer;
+	private float playerSpeed = 4.0f;
+	private float jumpHeight = 2.0f;
+	private float gravityValue = -7.0f; //change this to make it more floaty
+
+	private void Start()
+	{
+		controller = gameObject.GetComponent<CharacterController>();
+	}
+
+
+	void Update()
+	{
+		groundedPlayer = controller.isGrounded; //this uses the Character Controllers built in .isGrounded function to detect if the gameobject is on the ground.
+		if (groundedPlayer && playerVelocity.y <= 0)
+		{
+			playerVelocity.y = 0f;
+		}
+
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0); //we only need the horizontal axis since we are only moving left and right.
+		controller.Move(move * Time.deltaTime * playerSpeed);
+
+		//if move has any value at all...
+		if (move != Vector3.zero)
+		{
+			gameObject.transform.forward = move; 
+		}
+
+		// Changes the height position of the player..
+		if (Input.GetButton("Jump") && groundedPlayer)
+		{
+			playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+		}
+		playerVelocity.y += gravityValue * Time.deltaTime;
+		controller.Move(playerVelocity * Time.deltaTime);
+	}
+
+}
