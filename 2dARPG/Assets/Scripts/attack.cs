2242aea7c87c43fd85255ab34f0d70176780cd56
Andrew Miller <thedr111111@yahoo.com>
2020-08-13 15:29:11 -0700

    Implemented basic ranged attacks
    basic ranged attacks, issue with changing ranged attack directions.

--- 2dARPG/Assets/Scripts/attack.cs
+++ 2dARPG/Assets/Scripts/attack.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 //Insantiates a collision volume infront of the actor. When a target enters the volume damage is dealt.
 public class attack : MonoBehaviour
 {
-    public GameObject attackPattern; //this is the attack pattern prefab
+    public GameObject melee_AttackPattern; //this is the attack pattern prefab
+    public GameObject ranged_AttackPattern;
     GameObject childObject; //this is the object that will be instatinated, could be different attack patterns.
 
     public GameObject attackStartLocation; //this is an empty gameobject that we will instantiate the attack pattern on top of.
 
+
     bool swing = true; //controls whether the player can attack.
     float cooldownTime; //controls the time between attacks.
+    bool ranged = true;
 
     // Start is called before the first frame update
     void Start()
@@ -25,24 +28,44 @@ public class attack : MonoBehaviour
     {
         //if the fire button is tapped and the 
         if(Input.GetButtonDown("Fire1") && swing){
-            swing = false;
-            childObject = Instantiate(attackPattern, attackStartLocation.transform,false) as GameObject; //insantiates the attackpattern
-           // childObject.transform.SetParent(attackStartLocation.transform); //sets the transform of the attackpattern
-            
-            Debug.Log("FIRE");
-            StartCoroutine("rateOfAttack", 0.2f);
-
+            meleeAttack();
+        }
+        if(Input.GetButtonDown("Fire2") && swing)
+        {
+            rangedAttack();
         }
     }
 
+    //this function handels all melee attacks
+    void meleeAttack()
+    {
+        swing = false;
+        childObject = Instantiate(melee_AttackPattern, attackStartLocation.transform, false) as GameObject; //insantiates the attackpattern
+        childObject.transform.SetParent(attackStartLocation.transform); //sets the transform of the attackpattern
+        StartCoroutine("rateOfMeleeAttack", 0.2f);
+    }
+
+    void rangedAttack()
+    {
+        swing = false;
+        childObject = Instantiate(ranged_AttackPattern, attackStartLocation.transform, false) as GameObject; //insantiates the attackpattern
+        childObject.transform.SetParent(attackStartLocation.transform); //sets the transform of the attackpattern
+        StartCoroutine("rateOfFire", 0.2f);
+    }
 
 
-    IEnumerator rateOfAttack(float cooldown)
+    //melee attack cooroutine distroys the swing after a set amount of time
+    IEnumerator rateOfMeleeAttack(float cooldown)
     {
         yield return new WaitForSeconds(cooldown);
         Destroy(childObject);
         swing = true;
-        
     }
 
+    //rate of Fire just controls how long between ranged weapon attacks
+    IEnumerator rateOfFire(float cooldown)
+    {
+        yield return new WaitForSeconds(cooldown);
+        swing = true;
+    }
 }
---
+++ 2dARPG/Assets/bullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+//The Bullet will move the ranged_AttackPattern forward at a specific speed.
+
+public class bullet : MonoBehaviour
+{
+
+    float bulletSpeed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine("fire");
+    }
+
+    public void move()
+    {
+       // transform.parent.parent.InverseTransformDirection(transform.forward);
+        transform.Translate(Vector3.forward *bulletSpeed* Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("hit something");
+        Destroy(gameObject);
+    }
+
+    IEnumerator fire()
+    {
+        move();
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+    }
+
+}
